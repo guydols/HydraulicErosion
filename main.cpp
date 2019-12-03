3da4e677a3798d19f6adf376c53eca6fccdeb4a5
Guy Dols <guydols@gmail.com>
2019-12-03 11:58:14 +0100

    Cleaning up

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <chrono>
 #include "HydraulicErosion.h"
 #include "Tool.h"
 
@@ -9,34 +9,38 @@ int HEIGHT = 256;
 
 int main() {
 
-  std::vector<std::vector<float> > map;
-  Tool::resize2DVector(map, WIDTH, HEIGHT);
+    std::vector<std::vector<float> > map;
+    Tool::resize2DVector(map, WIDTH, HEIGHT);
 
-  for (int y = 0; y < HEIGHT; ++y) {
-    map[y] =  Tool::randomFloatsBetween(SEED, WIDTH, 0.0, 1.0);
-  }
+    for (int y = 0; y < HEIGHT; ++y) {
+        map[y] =  Tool::randomFloatsBetween(SEED, WIDTH, 0.0, 1.0);
+    }
 
-  HydraulicErosionConfig eroderConfig;
+    HydraulicErosionConfig eroderConfig;
 
-  eroderConfig.seed = SEED;
-  eroderConfig.numIterations = 10000;
-  eroderConfig.erosionRadius = 3;
-  eroderConfig.inertia = .3f;
-  eroderConfig.sedimentCapacityFactor = 8;
-  eroderConfig.minSedimentCapacity = .01f;
-  eroderConfig.erodeSpeed = .7f;
-  eroderConfig.depositSpeed = .2f;
-  eroderConfig.evaporateSpeed = .02f;
-  eroderConfig.gravity = 10;
-  eroderConfig.maxDropletLifetime = 64;
-  eroderConfig.initialWaterVolume = 1;
-  eroderConfig.initialVelocity = 1;
-  eroderConfig.mapWidth = WIDTH;
-  eroderConfig.mapHeight = HEIGHT;
+    eroderConfig.seed = SEED;
+    eroderConfig.numIterations = 10000;
+    eroderConfig.erosionRadius = 3;
+    eroderConfig.inertia = .3f;
+    eroderConfig.sedimentCapacityFactor = 8;
+    eroderConfig.minSedimentCapacity = .01f;
+    eroderConfig.erodeSpeed = .7f;
+    eroderConfig.depositSpeed = .2f;
+    eroderConfig.evaporateSpeed = .02f;
+    eroderConfig.gravity = 10;
+    eroderConfig.maxDropletLifetime = 32;
+    eroderConfig.initialWaterVolume = 1;
+    eroderConfig.initialVelocity = 1;
+    eroderConfig.mapWidth = WIDTH;
+    eroderConfig.mapHeight = HEIGHT;
 
-  HydraulicErosion eroder = HydraulicErosion(eroderConfig);
-  eroder.Simulate(map);
+    HydraulicErosion eroder = HydraulicErosion(eroderConfig);
 
-  map = Tool::shift2DVector(map, 0.0, 1.0);
+    auto start = std::chrono::high_resolution_clock::now();
 
+    eroder.Simulate(map);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_span = (stop - start);
+    std::cout << time_span.count() << std::endl;
 ;}
